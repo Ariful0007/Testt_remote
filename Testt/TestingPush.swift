49c066ee420c637d5096c984eae10e08c1b3fb15
Ariful0007 <arifulpub143@gmail.com>
2023-04-15 10:22:07 +0600

    second Test

---
+++ Testt/TestingPush.swift
@@ -0,0 +1,20 @@
+//
+//  TestingPush.swift
+//  Testt
+//
+//  Created by sang on 24/9/1444 AH.
+//
+
+import SwiftUI
+
+struct TestingPush: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestingPush_Previews: PreviewProvider {
+    static var previews: some View {
+        TestingPush()
+    }
+}
