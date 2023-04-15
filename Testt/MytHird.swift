913f23810bf477a65755d420d2f26b564ff1040e
Ariful0007 <arifulpub143@gmail.com>
2023-04-15 11:52:18 +0600

    Againupdate

---
+++ Testt/MytHird.swift
@@ -0,0 +1,20 @@
+//
+//  MytHird.swift
+//  Testt
+//
+//  Created by sang on 24/9/1444 AH.
+//
+
+import SwiftUI
+
+struct MytHird: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MytHird_Previews: PreviewProvider {
+    static var previews: some View {
+        MytHird()
+    }
+}
