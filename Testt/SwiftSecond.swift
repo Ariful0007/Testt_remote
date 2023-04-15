a0b2195f3432342af80f520dbaa4a23dbfc6644c
Ariful0007 <arifulpub143@gmail.com>
2023-04-15 10:14:50 +0600

    cording

---
+++ Testt/SwiftSecond.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftSecond.swift
+//  Testt
+//
+//  Created by sang on 24/9/1444 AH.
+//
+
+import SwiftUI
+
+struct SwiftSecond: View {
+    var body: some View {
+        Text("Ariful doing his job")
+    }
+}
+
+struct SwiftSecond_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftSecond()
+    }
+}
