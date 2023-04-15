5a0c5e34ae0dd635b1decb42fa7bd82701bbc03d
Ariful0007 <arifulpub143@gmail.com>
2023-04-15 09:48:23 +0600

    Initial Commit

---
+++ Testt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Testt
+//
+//  Created by sang on 24/9/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Testt/TesttApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesttApp.swift
+//  Testt
+//
+//  Created by sang on 24/9/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct TesttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
